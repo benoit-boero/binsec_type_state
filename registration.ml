38ef344541c534d027022b5986316bf403ab7b7f
Benoit Boero <benoitboero@gmail.com>
2026-01-13 11:32:46 +0100

    Initial commit.

---
+++ registration.ml
@@ -0,0 +1,8 @@
+module Namespace = Binsec_cli.Cli.Make (struct
+  let name="Type State"
+  let shortname="type_state"
+end)
+
+let () =
+  Binsec_cli_sse.Plugins.register ~is_enabled:Namespace.is_enabled (fun () -> 
+    (module Type_state.Plugin))
