1f5639fc685ace3cf894380249b83905366de1dc
Benoit Boero <benoitboero@gmail.com>
2026-02-10 10:06:16 +0100

    Updated cli to include the logger

--- registration.ml
+++ registration.ml
@@ -1,8 +1,11 @@
-module Namespace = Binsec_cli.Cli.Make (struct
-  let name="Type State"
-  let shortname="type-state"
-end)
+module Namespace =
+  Binsec_cli.Cli.Make_from_logger
+    (Type_state.TSLogger)
+    (struct
+      let name = "Type State"
+      let shortname = "type-state"
+    end)
 
 let () =
-  Binsec_cli_sse.Plugins.register ~is_enabled:Namespace.is_enabled (fun () -> 
-    (module Type_state.Plugin))
+  Binsec_cli_sse.Plugins.register ~is_enabled:Namespace.is_enabled (fun () ->
+      (module Type_state.Plugin))
